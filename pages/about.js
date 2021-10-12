b7ce4c49a62c5119db5d7bd269888ace07e9fe9c
Musab <musab.hussaink@gmail.com>
2021-10-12 17:22:41 +0500

    about page updated

--- pages/about.js
+++ pages/about.js
@@ -2,7 +2,7 @@ export default function AboutPage() {
     return (
         <div>
             <h1>About</h1>
-            <p>This is an app to find the latest DJ and other musical events</p>
+            <p>Welcome! This is an app to find the latest DJ and other musical events</p>
             <p>Version: 1.0.0</p>
         </div>
     )
